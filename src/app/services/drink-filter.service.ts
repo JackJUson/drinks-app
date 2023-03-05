27a458918688d606d05da46a1fab1c7c91478f95
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 10:22:03 +1100

    Add drinks property to drink filter service

--- src/app/services/drink-filter.service.ts
+++ src/app/services/drink-filter.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Drink } from '../Drink';
+
+import drinksData from '../json/data.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrinkFilterService {
+  drinks: Drink[] = drinksData;
 
   constructor() { }
 }
