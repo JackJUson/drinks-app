17fb35e0cb977868501065b57bc37d69ac8dd405
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 10:33:58 +1100

    Add drop down menu event handler for drink filter service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DrinkFilterService } from '../../services/drink-filter.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private drinkFilterService: DrinkFilterService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onFilterChange(event: Event): void {
-
+    const target = event.target as HTMLSelectElement;
+    const filterValue = target.value;
+    this.drinkFilterService.filterDrinks(filterValue);
   }
 }
