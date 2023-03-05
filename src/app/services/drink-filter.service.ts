66239128c38fb6f11dbdbb2227562437ace41bcb
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 10:40:42 +1100

    Written implementation for filterDrinks and getFilteredDrinks

--- src/app/services/drink-filter.service.ts
+++ src/app/services/drink-filter.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Drink } from '../Drink';
 
 import drinksData from '../json/data.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DrinkFilterService {
   drinks: Drink[] = drinksData;
 
+  filteredDrinks = new BehaviorSubject<Drink[]>([]);
+
   constructor() {}
 
   filterDrinks(value: string): void {
-    
+    const filteredDrinks = this.drinks.filter((drink) => drink.type === value);
+    this.filteredDrinks.next(filteredDrinks);
+  }
+
+  getFilteredDrinks(): Subject<any> {
+    return this.filteredDrinks;
   }
 }
