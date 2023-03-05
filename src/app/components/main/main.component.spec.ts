2cd3d10b6db3ef753c656e2519af9daebdca8387
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 10:54:21 +1100

    Update component declaration in main spec file

--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
@@ -8,7 +9,8 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      imports: [FormsModule],
+      declarations: [ MainComponent, HeaderComponent ]
     })
     .compileComponents();
 
