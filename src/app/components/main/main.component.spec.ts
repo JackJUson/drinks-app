0002e1830e738f4d7b9ac8aa1272c4e237c5f017
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 10:59:54 +1100

    Import jest globals

--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
 import { MainComponent } from './main.component';
+import { describe, expect, it, beforeEach } from '@jest/globals';
 
 describe('MainComponent', () => {
   let component: MainComponent;
