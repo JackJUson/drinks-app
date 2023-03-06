389cc48d0a0c2fe685add9397916d7c0b0fbd0e2
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 11:12:55 +1100

    Import drink filter service to call spyon method

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,19 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
-import { describe, expect, it, beforeEach } from '@jest/globals';
+import { describe, expect, it, beforeEach, jest } from '@jest/globals';
+import { DrinkFilterService } from '../../services/drink-filter.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let drinkFilterService: DrinkFilterService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [ DrinkFilterService ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    drinkFilterService = TestBed.inject(DrinkFilterService);
     fixture.detectChanges();
   });
 
