681d268f798bd9a15d45e64edbc26c66949a985e
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 11:03:10 +1100

    Import jest globals

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { describe, expect, it, beforeEach } from '@jest/globals';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
