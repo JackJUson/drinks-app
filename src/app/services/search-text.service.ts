cfeedd9225c9544cb0f1e90292ac42cf0721c81a
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 11:43:49 +1100

    Add search source property to share search text

--- src/app/services/search-text.service.ts
+++ src/app/services/search-text.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchTextService {
+  private searchSource = new BehaviorSubject<string>('');
+  searchText = this.searchSource.asObservable();
 
   constructor() { }
 }
