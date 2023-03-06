8f0ab3c0f96c1acecdccc0f72183fa01e24f7236
Jackson <z5397791@ad.unsw.edu.au>
2023-03-06 11:22:16 +1100

    Import jest globals

--- src/app/services/drink-filter.service.spec.ts
+++ src/app/services/drink-filter.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DrinkFilterService } from './drink-filter.service';
+import { describe, expect, it, beforeEach } from '@jest/globals';
 
 describe('DrinkFilterService', () => {
   let service: DrinkFilterService;
@@ -13,4 +14,6 @@ describe('DrinkFilterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('filterDrinks tests', () => {});
 });
